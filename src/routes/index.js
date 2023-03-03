77c1987965c0364644676b4af560ff2376c3f5f3
ThangNguyenT5 <toanthangttp@gmail.com>
2023-03-03 17:52:40 +0700

    Cấu hình base hoàn thành

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
 import Upload from '~/pages/Upload';
+
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
-    { path: '/upload', component: Upload, layout: null },
+    { path: '/search', component: Search, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
 
 const privateRoutes = [];
